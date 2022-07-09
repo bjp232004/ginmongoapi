14bb25d00f76afadfe0466421e7bd6183eff7b03
Ubuntu <ubuntu@ip-172-31-22-204.ap-south-1.compute.internal>
2022-07-09 07:33:46 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"ginmongoapi/configs"
+	"ginmongoapi/routes" //add this
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	//run database
+	configs.ConnectDB()
+
+	//routes
+	routes.UserRoute(router) //add this
+
+	router.Run("localhost:8080")
+}
---
+++ routes/user_route.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"ginmongoapi/controllers" //add this
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoute(router *gin.Engine) {
+	router.POST("/user", controllers.CreateUser())            //add this
+	router.GET("/user/:userId", controllers.GetAUser())       //add this
+	router.PUT("/user/:userId", controllers.EditAUser())      //add this
+	router.DELETE("/user/:userId", controllers.DeleteAUser()) //add this
+	router.GET("/users", controllers.GetAllUsers())           //add this
+
+}
