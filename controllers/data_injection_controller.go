f8e56d87934699e642df8371c89e4e4aca201ab8
Ubuntu <ubuntu@ip-172-31-22-204.ap-south-1.compute.internal>
2022-07-28 16:52:27 +0000

    Create Index and search data

--- controllers/data_injection_controller.go
+++ controllers/data_injection_controller.go
@@ -1,14 +1,76 @@
 package controllers
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/blugelabs/bluge"
 	"github.com/gin-gonic/gin"
 )
+
+var bludgePath = "/home/ubuntu/projects/ginmongoapi/blugedump/"
+var folderRootPath = "/home/ubuntu/projects/ginmongoapi/blugedata/"
+
+type userObject struct {
+	ID   string
+	Name string
+}
+
+func CreateZincIndex() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		config := bluge.DefaultConfig(bludgePath)
+		writer, err := bluge.OpenWriter(config)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+		defer writer.Close()
+
+		files, err := ioutil.ReadDir(folderRootPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			fileName := folderRootPath + f.Name()
+			fmt.Println(fileName)
+
+			doc := bluge.NewDocument(fileName)
+			docs, err := ioutil.ReadFile(fileName)
+
+			if err != nil {
+				fmt.Printf("error while readfile: %v", err)
+				os.Exit(1)
+			}
+
+			var users []userObject
+
+			err = json.Unmarshal(docs, &users)
+			if err != nil {
+				fmt.Printf("error while unmarshal: %v", err)
+				os.Exit(1)
+			}
+
+			for k := range users {
+				doc.AddField(bluge.NewTextField("id", users[k].ID))
+				doc.AddField(bluge.NewTextField("name", users[k].Name))
+				err = writer.Update(doc.ID(), doc)
+			}
+
+			if err != nil {
+				fmt.Printf("error updating document: %v", err)
+				os.Exit(1)
+			}
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "Index has been created."})
+	}
+}
 
 func UploadFile() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -34,6 +96,177 @@ func UploadFile() gin.HandlerFunc {
 
 func FileUpload() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "select_file.html", gin.H{})
+		matchFlag := "Not Match"
+		searchColumn := c.Param("field")
+		searchTerm := c.Param("value")
+		fmt.Printf("Search Term: %s", searchTerm)
+		config := bluge.DefaultConfig(bludgePath)
+		writer, err := bluge.OpenWriter(config)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+		defer writer.Close()
+
+		files, err := ioutil.ReadDir(folderRootPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			fileName := folderRootPath + f.Name()
+			fmt.Println(fileName)
+
+			doc := bluge.NewDocument(fileName)
+			docs, err := ioutil.ReadFile(fileName)
+
+			if err != nil {
+				fmt.Printf("error while readfile: %v", err)
+				os.Exit(1)
+			}
+
+			var users []userObject
+
+			err = json.Unmarshal(docs, &users)
+			if err != nil {
+				fmt.Printf("error while unmarshal: %v", err)
+				os.Exit(1)
+			}
+
+			for k := range users {
+				doc.AddField(bluge.NewTextField("id", users[k].ID))
+				doc.AddField(bluge.NewTextField("name", users[k].Name))
+				err = writer.Update(doc.ID(), doc)
+			}
+
+			if err != nil {
+				fmt.Printf("error updating document: %v", err)
+				os.Exit(1)
+			}
+		}
+
+		//QUery Index
+		reader, err := writer.Reader()
+		if err != nil {
+			fmt.Printf("error getting index reader: %v", err)
+		}
+		defer reader.Close()
+
+		query := bluge.NewMatchQuery(searchTerm).SetField(searchColumn)
+
+		request := bluge.NewTopNSearch(100, query).
+			WithStandardAggregations()
+		documentMatchIterator, err := reader.Search(context.Background(), request)
+		if err != nil {
+			fmt.Printf("error executing search: %v", err)
+		}
+		match, err := documentMatchIterator.Next()
+		for err == nil && match != nil {
+			err = match.VisitStoredFields(func(field string, value []byte) bool {
+				if field == "_id" {
+					matchFlag = "Matched"
+					fmt.Printf("match found: %s\n", string(value))
+				}
+				return true
+			})
+			if err != nil {
+				fmt.Printf("error loading stored fields: %v", err)
+			}
+			match, err = documentMatchIterator.Next()
+		}
+		if err != nil {
+			fmt.Printf("error iterator document matches: %v", err)
+		}
+		c.JSON(http.StatusOK, gin.H{"match found end": matchFlag})
+	}
+}
+
+func QueryData() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		config := bluge.DefaultConfig(bludgePath)
+		writer, _ := bluge.OpenWriter(config)
+		reader, err := writer.Reader()
+		if err != nil {
+			fmt.Printf("error getting index reader: %v", err)
+			// log.Fatalf("error getting index reader: %v", err)
+		}
+		defer reader.Close()
+
+		query := bluge.NewMatchQuery("david").SetField("firstName")
+		fmt.Print(query)
+		request := bluge.NewTopNSearch(10, query).
+			WithStandardAggregations()
+		documentMatchIterator, err := reader.Search(context.Background(), request)
+		if err != nil {
+			fmt.Printf("error executing search: %v", err)
+			// log.Fatalf("error executing search: %v", err)
+		}
+		match, err := documentMatchIterator.Next()
+		for err == nil && match != nil {
+			err = match.VisitStoredFields(func(field string, value []byte) bool {
+				if field == "_id" {
+					fmt.Printf("match found: %s\n", string(value))
+				}
+				return true
+			})
+			if err != nil {
+				fmt.Printf("error loading stored fields: %v", err)
+				// log.Fatalf("error loading stored fields: %v", err)
+			}
+			match, err = documentMatchIterator.Next()
+		}
+		if err != nil {
+			fmt.Printf("error iterator document matches: %v", err)
+			// log.Fatalf("error iterator document matches: %v", err)
+		}
+		c.JSON(http.StatusOK, gin.H{"match found": match})
+	}
+}
+
+func SearchData() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		matchFlag := "Not Match"
+		searchColumn := c.Param("field")
+		searchTerm := c.Param("value")
+		fmt.Printf("Search Term: %s", searchTerm)
+		config := bluge.DefaultConfig(bludgePath)
+		writer, err := bluge.OpenWriter(config)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+		defer writer.Close()
+
+		//QUery Index
+		reader, err := writer.Reader()
+		if err != nil {
+			fmt.Printf("error getting index reader: %v", err)
+		}
+		defer reader.Close()
+
+		query := bluge.NewMatchQuery(searchTerm).SetField(searchColumn)
+
+		request := bluge.NewTopNSearch(100, query).
+			WithStandardAggregations()
+		documentMatchIterator, err := reader.Search(context.Background(), request)
+		if err != nil {
+			fmt.Printf("error executing search: %v", err)
+		}
+		match, err := documentMatchIterator.Next()
+		for err == nil && match != nil {
+			err = match.VisitStoredFields(func(field string, value []byte) bool {
+				if field == "_id" {
+					matchFlag = "Matched"
+					fmt.Printf("match found: %s\n", string(value))
+				}
+				return true
+			})
+			if err != nil {
+				fmt.Printf("error loading stored fields: %v", err)
+			}
+			match, err = documentMatchIterator.Next()
+		}
+		if err != nil {
+			fmt.Printf("error iterator document matches: %v", err)
+		}
+		c.JSON(http.StatusOK, gin.H{"match found end": matchFlag})
 	}
 }
--- routes/data_injection_route.go
+++ routes/data_injection_route.go
@@ -8,5 +8,8 @@ import (
 
 func DataInjectionRoute(router *gin.Engine) {
 	router.POST("/upload", controllers.UploadFile())
-	router.GET("/file_upload", controllers.FileUpload())
+	router.GET("/file_upload/:field/:value", controllers.FileUpload())
+	router.GET("/query_data", controllers.QueryData())
+	router.GET("/create_index", controllers.CreateZincIndex())
+	router.GET("/search/:field/:value", controllers.SearchData())
 }
