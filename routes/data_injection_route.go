1269428e2ccf959e8baf00f29ef367379e2fc132
Ubuntu <ubuntu@ip-172-31-22-204.ap-south-1.compute.internal>
2022-07-28 16:53:58 +0000

    remove unwanted routes

--- routes/data_injection_route.go
+++ routes/data_injection_route.go
@@ -8,8 +8,6 @@ import (
 
 func DataInjectionRoute(router *gin.Engine) {
 	router.POST("/upload", controllers.UploadFile())
-	router.GET("/file_upload/:field/:value", controllers.FileUpload())
-	router.GET("/query_data", controllers.QueryData())
 	router.GET("/create_index", controllers.CreateZincIndex())
 	router.GET("/search/:field/:value", controllers.SearchData())
 }
